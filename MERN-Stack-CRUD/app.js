c1723e1c5d564a1e8039f8448b4268b4d0a9c36b
Mohammed <mohammedbakkali807@gmail.com>
2024-08-03 20:42:35 +0100

    update

--- MERN-Stack-CRUD/app.js
+++ MERN-Stack-CRUD/app.js
@@ -2,12 +2,27 @@ const express = require("express");
 const app = express();
 const port = 3001;
 const mongoose = require("mongoose");
+app.use(express.urlencoded({ extended: true }));
+const Mydata = require("./models/MyDataSchema");
+app.set("view engine", "ejs");
 
+// Process the GET request for the home page
 app.get("/", (req, res) => {
-  res.sendFile("./views/home.html", { root: __dirname });
+  // result ==> array of object
+  Mydata.find()
+    .then((result) => {
+      res.render("home", { mytitle: "Home page", arr: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
+
+// Connect to the database
 mongoose
-  .connect("mongodb+srv://Mohammed:A9Kc0kaA34pR0APO@cluster0.umktxvb.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0")
+  .connect(
+    "mongodb+srv://Mohammed:A9Kc0kaA34pR0APO@cluster0.umktxvb.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => {
     app.listen(port, () => {
       console.log(`http://localhost:${port}`);
@@ -16,3 +31,20 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+// Process the POST request for data
+app.post("/", (req, res) => {
+  console.log(req.body);
+
+  const mydata = new Mydata(req.body);
+
+  mydata
+    .save()
+    .then(() => {
+      res.redirect("/index.html");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Error saving data to database.");
+    });
+});
