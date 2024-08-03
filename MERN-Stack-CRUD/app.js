a032c6b564a7c79aaacee656938537367b87e9e6
Mohammed <mohammedbakkali807@gmail.com>
2024-08-03 03:26:35 +0100

    update

--- MERN-Stack-CRUD/app.js
+++ MERN-Stack-CRUD/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.sendFile("./views/home.html", { root: __dirname });
 });
-
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`);
-});
+mongoose
+  .connect("mongodb+srv://Mohammed:A9Kc0kaA34pR0APO@cluster0.umktxvb.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
